54790203950da38a6209366f7ce7f6e8223d32a0
hannarosenh <162818141+hannarosenh@users.noreply.github.com>
2024-03-09 16:29:48 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const init = () => {
+    const $hamburger = document.querySelector('.hamburger');
+    const $nav = document.querySelector('.nav__list');
+
+    $hamburger.addEventListener("click", () => {
+        $hamburger.classList.toggle("active");
+        $nav.classList.toggle("active");
+
+    })
+
+    document.querySelectorAll("nav__list-item__link").forEach(n => n.addEventListener("click", () => {
+        $hamburger.classList.remove("active");
+        $nav.classList.remove("active");
+    }))
+}
+
+init();
